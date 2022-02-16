ba615179ed42522cc814c86b8b1814961d125c85
Alex Ferreira <aferreira91@gmail.com>
2022-02-16 08:52:46 -0300

    lista

--- src/br/edu/uni7/aed2/Main.java
+++ src/br/edu/uni7/aed2/Main.java
@@ -2,13 +2,28 @@ package br.edu.uni7.aed2;
 
 import br.edu.uni7.aed2.listadupla.ListaDupla;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.
+
+
         ListaDupla listaDupla = new ListaDupla();
-        listaDupla.inserir(89);
-        listaDupla.inserir(2937);
-        listaDupla.imprimir();
+
+        for (int i = 0; i < 1_000_000; i++) {
+            listaDupla.inserir(i);
+        }
+
+        //listaDupla.imprimir();
+
+        long inicio = System.currentTimeMillis();
+        listaDupla.reverter();
+        long fim = System.currentTimeMillis();
+        long tempoTotal = fim - inicio;
+        System.out.println(tempoTotal + " ms");
     }
 
 }
--- src/br/edu/uni7/aed2/listadupla/ListaDupla.java
+++ src/br/edu/uni7/aed2/listadupla/ListaDupla.java
@@ -4,6 +4,13 @@ public class ListaDupla {
 
     No inicio;
     No fim;
+    int tamanho;
+
+    public ListaDupla() {
+        inicio = null;
+        fim = null;
+        tamanho = 0;
+    }
 
     public void inserir(int valor) {
         No no = new No();
@@ -16,8 +23,63 @@ public class ListaDupla {
             no.proximo = null;
             fim.proximo = no;
         }
-
         fim = no;
+
+        tamanho++;
     }
 
+    public void imprimir() {
+        No noAtual = inicio;
+        while (noAtual != null) {
+            System.out.println(noAtual.valor);
+            noAtual = noAtual.proximo;
+        }
+    }
+
+    public int valorNaPosicao(int posicao) {
+        No noAtual = inicio;
+        while (noAtual != null) {
+            if (posicao == 0) {
+                return noAtual.valor;
+            }
+            posicao--;
+            noAtual = noAtual.proximo;
+        }
+
+        throw new ArrayIndexOutOfBoundsException();
+    }
+
+    public void valorNaPosicao(int posicao, int valor) {
+        No noAtual = inicio;
+        while (noAtual != null) {
+            if (posicao == 0) {
+                noAtual.valor = valor;
+                break;
+            }
+            posicao--;
+            noAtual = noAtual.proximo;
+        }
+    }
+
+
+    public void reverter() {
+        //2.9 segundos
+//        int[] array = new int[tamanho];
+//
+//        int j = 0;
+//        for (int i = tamanho - 1; i >= 0; i--) {
+//            array[j] = valorNaPosicao(i);
+//            j++;
+//        }
+
+
+        for (int i = 0; i < tamanho / 2; i++) {
+            int valorNoFinal = valorNaPosicao(tamanho - 1 - i);
+            int valorNoInicio = valorNaPosicao(i);
+
+            valorNaPosicao(i, valorNoFinal);
+            valorNaPosicao(tamanho - 1 - i, valorNoInicio);
+        }
+
+    }
 }
