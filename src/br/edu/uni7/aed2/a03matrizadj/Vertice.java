0247d685ca0cce56d13b2b8a8f2402b3ddff93a2
Alex Ferreira <aferreira91@gmail.com>
2022-03-08 08:00:02 -0300

    lista e matriz

---
+++ src/br/edu/uni7/aed2/a03matrizadj/Vertice.java
@@ -0,0 +1,28 @@
+package br.edu.uni7.aed2.a03matrizadj;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Vertice {
+
+    private final Integer valor;
+    private final List<Vertice> vizinhos;
+
+    public Vertice(Integer valor) {
+        this.valor = valor;
+        vizinhos = new LinkedList<>();
+    }
+
+    public Integer getValor() {
+        return valor;
+    }
+
+    public List<Vertice> getVizinhos() {
+        return vizinhos;
+    }
+
+    public void adicionar(Vertice vizinho) {
+        vizinhos.add(vizinho);
+    }
+
+}
